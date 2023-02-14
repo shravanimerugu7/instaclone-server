fc7d46c1255eaacc155fd06668e5270335328d8b
shravanimerugu7 <shravanimerugu57@gmail.com>
2023-02-14 17:09:48 +0530

    adding backend routes file

---
+++ routes.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const router= express.Router();
+const Post = require('./users')
+//const post = mongoose.model("posts")
+
+
+router.route("/posts").post(async (req,res)=>{
+    const name= req.body.name;
+    const location=req.body.location;
+    const likes=req.body.likes;
+    const description=req.body.description;
+    const PostImage= req.body.PostImage;
+    const date=req.body.date;
+    const newpost = new Post({
+        name,
+        location,
+        likes,
+        description,
+        PostImage,
+        date
+    });
+   
+    try{
+        let user= await Post.insertMany(newpost)
+        res.json({status:"success",result:user})
+        //console.log(user);
+    }
+    catch(e){
+        res.json({status:"failed",result:e.message})
+    }
+
+});
+
+router.route("/posts").get( async (req,res)=>{
+   await Post.find()
+    .then((data)=>res.json(data))
+    //console.log(req.data)
+});
+
+
+module.exports= router;
